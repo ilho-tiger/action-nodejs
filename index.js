849744ab007416e7839d4fdf13d8e9c5b2a7e8b1
Ilho Song <ilho.song@siemens.com>
2020-03-24 10:01:14 -0400

    feat: add env var to control slack messege sending

--- index.js
+++ index.js
@@ -9,25 +9,52 @@ if (!process.env.slack_webhook) {
     console.log("No slack_webhook URL found");
     process.exit(1)
 }
-let slack_webhook = process.env.slack_webhook
+let slack_webhook = process.env.slack_webhook;
+
+let enableSlack = false;
+if (process.env.action_slack !== undefined && process.env.action_slack === 'true') {
+    enableSlack = true;
+    console.log("action_slack is set to true: Slack message enabled");
+}
+else {
+    enableSlack = false;
+    console.log("action_slack is set to false: Slack message disabled");
+}
+
+function addPrefix(str, prefix) {
+    let tmp = str.split('\n'),
+        res = [];
+
+    for (const frag of tmp) {
+        res.push(`${prefix} ${frag}`);
+    }
+
+    return res.join('\n');
+}
 
 function sendSlackMessage(message, incoming_webhook_url) {
-    request.post(
-        incoming_webhook_url,
-        {
-            json: {
-                text: message
+    if (enableSlack === true) {
+        request.post(
+            incoming_webhook_url,
+            {
+                json: {
+                    text: message
+                }
+            },
+            function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body);
+                }
+                else {
+                    console.log("error: " + error);
+                }
             }
-        },
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(body);
-            }
-            else {
-                console.log("error: " + error);
-            }
-        }
-    )
+        );
+    }
+    else {
+        message = addPrefix(message, "onSlack > ")
+        console.log(message);
+    }
 }
 
 function getTwoDigitPaddedNumberString(number) {
@@ -78,7 +105,6 @@ let getKoreaStatus = function () {
             }
         });
 
-        console.log(resultString);
         sendSlackMessage(resultString, slack_webhook);
     });
 };
@@ -138,7 +164,6 @@ let getUsStatus = function () {
 
         resultString += getStatMessageString(totalNumbersInUs) + "\n";
         resultString += getStatMessageString(numbersInGa, " in GA")
-        console.log(resultString);
         sendSlackMessage(resultString, slack_webhook);
     });
 }
@@ -168,9 +193,9 @@ let getGaStatus = function () {
                     gaTotal[split[0]] = split[1];
                 });
             }
-            else if(caption.includes('by County')){
+            else if (caption.includes('by County')) {
                 let counties = [];
-                $(this).find('tbody').find('tr').each(function(){
+                $(this).find('tbody').find('tr').each(function () {
                     let element = $(this).text();
                     let split = element.split('\n');
                     counties.push(split);
@@ -179,9 +204,9 @@ let getGaStatus = function () {
             }
         });
 
-        $(".body-content").children().find("em").each(function(){
+        $(".body-content").children().find("em").each(function () {
             let element = $(this).text();
-            if(element.includes("generated on:")){
+            if (element.includes("generated on:")) {
                 reportGenerated = element.split(': ')[1] + " EDT";
             }
         })
